267ed017c0cb3040c73866723c7df02be04d0d91
Argo Roots <argo@roots.ee>
2022-01-27 18:27:04 +0200

    Remove ESTEID-SK_2011.pem.crt

--- id-card/certs.sh
+++ id-card/certs.sh
@@ -7,7 +7,6 @@ rm ./certs/*.crt
 wget -qnv -P ./certs https://c.sk.ee/EE-GovCA2018.pem.crt
 wget -qnv -P ./certs https://c.sk.ee/esteid2018.pem.crt
 wget -qnv -P ./certs https://www.sk.ee/upload/files/EE_Certification_Centre_Root_CA.pem.crt
-wget -qnv -P ./certs https://www.sk.ee/upload/files/ESTEID-SK_2011.pem.crt
 wget -qnv -P ./certs https://www.skidsolutions.eu/upload/files/ESTEID-SK_2015.pem.crt
 
 cat ./certs/*.crt > ./id-card.pem.crt
