b89761cacf783f2f49e4e2b24e400abf5da6f396
Argo Roots <argo@roots.ee>
2022-01-28 18:38:24 +0200

    #15 Require authorization_code parameter

--- src/routes/token.js
+++ src/routes/token.js
@@ -1,9 +1,15 @@
 const storage = require('../storage.js')
 
 async function getToken (headers, params, res) {
+  if (params.grant_type !== 'authorization_code') {
+    res.writeHead(400, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ error: 'Parameter grant_type must be "authorization_code"' }))
+    return
+  }
+
   if (!params.code) {
     res.writeHead(400, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ error: 'Code is required' }))
+    res.end(JSON.stringify({ error: 'Parameter code is required' }))
     return
   }
 
