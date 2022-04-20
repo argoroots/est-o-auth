cc0bb82b16e0ff34e23f3062a9b9c8886539d30a
Argo Roots <argo@roots.ee>
2022-04-20 19:26:45 +0300

    #28 Create FormInput component

---
+++ web/src/components/FormInput.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { computed, defineEmits, defineProps } from 'vue'
+
+const props = defineProps({
+  modelValue: { type: String, default: '' },
+  id: { type: String, required: true },
+  label: { type: String, required: true },
+  type: { type: String, default: 'text' },
+  autofocus: { type: Boolean, default: false },
+  placeholder: { type: String, required: true }
+})
+
+const emit = defineEmits([
+  'update:modelValue',
+  'submit'
+])
+
+const text = computed({
+  get () {
+    return props.modelValue
+  },
+  set (val) {
+    emit('update:modelValue', val)
+  }
+})
+
+</script>
+
+<template>
+  <input
+    :id="id"
+    v-model="text"
+    :type="type"
+    :placeholder="placeholder"
+    :autofocus="autofocus"
+    class="peer h-10 px-2 w-full border border-slate-300 text-slate-900 placeholder-transparent focus:outline-none focus:border-slate-500"
+    @keypress.enter="emit('submit')"
+  >
+  <label
+    :for="id"
+    class="absolute left-1 px-1 -top-2.5 text-slate-500 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-slate-500 peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-slate-500 peer-focus:text-sm bg-white"
+  >{{ label }}</label>
+</template>
--- web/src/views/AuthEmail.vue
+++ web/src/views/AuthEmail.vue
@@ -6,18 +6,16 @@
 <template>
   <div class="p-4">
     <div class="relative">
-      <input
-        name="email"
+      <form-input
+        id="email"
+        label="Email address"
         type="email"
-        class="peer h-10 px-2 w-full border border-slate-300 text-slate-900 placeholder-transparent focus:outline-none focus:border-slate-500"
         placeholder="jaak-kristjan@jõeorg.ee"
         autofocus
+      />
+      <button
+        class="w-full mt-4 h-10 px-2 border border-slate-300 hover:bg-slate-100 active:bg-slate-200"
       >
-      <label
-        for="email"
-        class="absolute left-1 px-1 -top-2.5 text-slate-500 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-slate-500 peer-placeholder-shown:top-2 peer-focus:-top-2.5 peer-focus:text-slate-500 peer-focus:text-sm bg-white"
-      >Email address</label>
-      <button class="w-full mt-4 h-10 px-2 border border-slate-300 hover:bg-slate-100 active:bg-slate-200">
         Authenticate
       </button>
     </div>
