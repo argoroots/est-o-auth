aeae2c6c178a3e72f4010876c305b3d3709d0fc6
Argo Roots <argo@roots.ee>
2022-04-20 14:43:42 +0300

    Add icons

--- web/src/views/Auth.vue
+++ web/src/views/Auth.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router'
+import { ApplicationMobile, Email, SimCard } from '@vicons/carbon'
+import SmartId from '@/components/SmartId.vue'
 
 const route = useRoute()
 const methods = (route.query?.methods || 'e-mail,smart-id,mobile-id,id-card').split(',')
@@ -11,24 +13,28 @@ const methods = (route.query?.methods || 'e-mail,smart-id,mobile-id,id-card').sp
       v-if="methods.includes('e-mail')"
       to="/auth/e-mail"
     >
+      <email class="h-5 w-5" />
       E-mail
     </router-link>
     <router-link
       v-if="methods.includes('smart-id')"
       to="/auth/smart-id"
     >
+      <smart-id class="h-5 w-5" />
       Smart-ID
     </router-link>
     <router-link
       v-if="methods.includes('mobile-id')"
       to="/auth/mobile-id"
     >
+      <application-mobile class="h-5 w-5" />
       Mobile-ID
     </router-link>
     <a
       v-if="methods.includes('id-card')"
       href="/auth/id-card"
     >
+      <sim-card class="h-5 w-5" />
       ID-Card
     </a>
   </div>
@@ -36,6 +42,6 @@ const methods = (route.query?.methods || 'e-mail,smart-id,mobile-id,id-card').sp
 
 <style scoped>
 a {
-  @apply block py-4 px-6 border-b border-slate-100 hover:text-slate-900
+  @apply flex items-center gap-x-3 py-4 px-6 border-b border-slate-100 hover:text-slate-900
 }
 </style>
