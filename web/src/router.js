71eb3a7f557ddc9285540e42a12de0446231a494
Argo Roots <argo@roots.ee>
2022-04-20 14:03:15 +0300

    Redirect / to /auth

--- web/src/router.js
+++ web/src/router.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    redirect: '/auth'
+  },
+  {
     path: '/auth',
     name: 'Auth',
     component: () => import('@/views/Auth.vue')
