69b04c7752c2bd64a696bdaf7389d98cb495511f
Argo Roots <argo@roots.ee>
2022-04-20 19:27:21 +0300

    #28 Add auth logic

--- web/src/views/AuthEmail.vue
+++ web/src/views/AuthEmail.vue
@@ -1,6 +1,25 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+import formInput from '@/components/FormInput.vue'
+
+const route = useRoute()
+const email = ref(route.query?.email)
+const emailSent = ref(false)
+
+if (email.value) {
+  onAuthenticate()
+}
+
+function onAuthenticate () {
+  if (!email.value?.trim()) {
+    return
+  }
+
+  emailSent.value = true
+  console.log(email.value)
+}
 </script>
 
 <template>
@@ -8,13 +27,16 @@
     <div class="relative">
       <form-input
         id="email"
+        v-model="email"
         label="Email address"
         type="email"
         placeholder="jaak-kristjan@jõeorg.ee"
         autofocus
+        @submit="onAuthenticate"
       />
       <button
         class="w-full mt-4 h-10 px-2 border border-slate-300 hover:bg-slate-100 active:bg-slate-200"
+        @click="onAuthenticate"
       >
         Authenticate
       </button>
