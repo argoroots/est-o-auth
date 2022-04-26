71a9d3ebcec5c2280acf0366dbd4e4894552a548
Argo Roots <argo@roots.ee>
2022-04-26 11:43:57 +0300

    Cleanup storage helper code

--- api/routes/_storage.js
+++ api/routes/_storage.js
@@ -1,49 +1,45 @@
+const { createClient } = require('redis')
 const crypto = require('crypto')
-const redis = require('redis')
 const jwt = require('jsonwebtoken')
 
-const client = redis.createClient({ url: process.env.REDIS })
+const redis = createClient({ url: process.env.REDIS })
 
-client.on('error', (err) => console.log('Redis Client Error', err))
+redis.on('error', (err) => console.log('Redis Client Error', err))
 
-async function saveEmail (email) {
+export async function saveEmail (email) {
   const numbers = '000000' + Math.round(Math.random() * 1000000)
   const code = numbers.slice(numbers.length - 6)
 
-  await client.connect()
-  await client.set(`email:${email}:${code}`, JSON.stringify({ email }))
-  await client.disconnect()
+  await redis.connect()
+  await redis.set(`email:${email}:${code}`, JSON.stringify({ email }))
+  await redis.disconnect()
 
   return code
 }
 
-async function saveUser (user) {
+export async function saveUser (user) {
   const code = crypto.randomUUID().replaceAll('-', '')
 
-  await client.connect()
-  await client.set('user:' + code, JSON.stringify(user))
-  await client.disconnect()
+  await redis.connect()
+  await redis.set('user:' + code, JSON.stringify(user))
+  await redis.disconnect()
 
   return code
 }
 
-async function getToken (code, expiresIn) {
-  await client.connect()
-  const user = await client.get('user:' + code)
+export async function getToken (code, expiresIn) {
+  await redis.connect()
+
+  const key = `user:${code}`
+  const user = await redis.get(key)
 
   if (!user) {
-    await client.disconnect()
+    await redis.disconnect()
     return
   }
 
-  await client.del('user:' + code)
-  await client.disconnect()
+  await redis.del(key)
+  await redis.disconnect()
 
   return jwt.sign(JSON.parse(user), process.env.JWT_SECRET, { expiresIn })
-}
-
-module.exports = {
-  saveEmail,
-  saveUser,
-  getToken
 }
