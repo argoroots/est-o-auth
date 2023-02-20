89a970534ab1034b3973fc95e28fdba4b9363ef2
Argo Roots <argo@roots.ee>
2023-02-20 14:38:37 +0200

    Fix api get method

--- web/src/api.js
+++ web/src/api.js
@@ -1,10 +1,7 @@
 export async function get (pathname, params) {
-  const url = new URL(import.meta.env.VITE_APP_API_URL || '')
+  const search = new URLSearchParams(params).toString()
 
-  url.pathname = '/api/' + pathname
-  url.search = new URLSearchParams(params).toString()
-
-  return await fetch(url).then(response => response.json())
+  return await fetch(`${import.meta.env.VITE_APP_API_URL || ''}/api/${pathname}?${search}`).then(response => response.json())
 }
 
 export async function post (pathname, body) {
