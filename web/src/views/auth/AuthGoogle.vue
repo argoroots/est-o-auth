1b5cad9c8e3e0fd975b3c25235e97fe7810d26c0
Argo Roots <argo@roots.ee>
2023-02-20 23:59:57 +0200

    Fix url

--- web/src/views/auth/AuthGoogle.vue
+++ web/src/views/auth/AuthGoogle.vue
@@ -1,3 +1,3 @@
 <script setup>
-window.location.href = `${import.meta.env.VITE_APP_API_URL}/api/google${window.location.search}`
+window.location.href = `${import.meta.env.VITE_APP_API_URL || ''}/api/google${window.location.search}`
 </script>
