ddcfb86887db1134fd70ddab8dbc2fe9fb70e2bd
Argo Roots <argo@roots.ee>
2023-02-21 02:19:16 +0200

    Update AuthPage.vue

--- web/src/views/AuthPage.vue
+++ web/src/views/AuthPage.vue
@@ -14,7 +14,7 @@ const { meta } = useRoute()
       <footer>
         <p
           v-if="meta.description"
-          class="px-8 pt-16 text-center"
+          class="px-8 pt-16"
         >
           {{ meta.description }}
         </p>
