715cfb2604c736bc13468a50461a1019a2bd0500
Argo Roots <argo@roots.ee>
2023-03-10 16:31:41 +0200

    Log nonce

--- web/src/views/auth/WebEid.vue
+++ web/src/views/auth/WebEid.vue
@@ -5,9 +5,11 @@ import { authenticate } from '@web-eid/web-eid-library/web-eid.js'
 onMounted(async () => {
   const { nonce } = await fetch('https://oauth.ee/api/web-eid/nonce')
 
+  console.log('nonce', nonce)
+
   const authToken = await authenticate(nonce, { lang: 'et' })
 
-  console.log(authToken)
+  console.log('authToken', authToken)
 })
 </script>
 
