5d2fe9d5edc1bf18e2aa0e9d5a921e2b3bd6c184
Argo Roots <argo@roots.ee>
2023-03-10 16:23:09 +0200

    Add /auth/web-eid endpoint

---
+++ web/src/views/auth/WebEid.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from 'vue'
+import { authenticate } from '@web-eid/web-eid-library/web-eid.js'
+
+onMounted(async () => {
+  const { nonce } = await fetch('https://oauth.ee/api/web-eid/nonce')
+
+  const authToken = await authenticate(nonce, { lang: 'et' })
+
+  console.log(authToken)
+})
+</script>
+
+<template>
+  <div />
+</template>
